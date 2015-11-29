61d700c9b4b41607f239ab1e8b03bc469fa5df05
Chen Yu <chenyu@shishuo.com>
2015-11-29 21:47:35 +0800

    实现散列表和雷达效果

---
+++ app/src/main/java/com/example/chenyu/mpandroidcharttest/PieCharFragment.java
@@ -0,0 +1,7 @@
+package com.example.chenyu.mpandroidcharttest;
+
+/**
+ * Created by Think on 2015/11/29.
+ */
+public class PieCharFragment {
+}
---
+++ app/src/main/java/com/example/chenyu/mpandroidcharttest/RadarCharFragment.java
@@ -0,0 +1,75 @@
+package com.example.chenyu.mpandroidcharttest;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Think on 2015/11/29.
+ */
+public class RadarCharFragment extends Fragment {
+    public LineChart lineChart;
+    public ArrayList<String> x=new ArrayList<String>();
+    public ArrayList<Entry> y=new ArrayList<Entry>();
+    public ArrayList<LineDataSet> lineDataSets=new ArrayList<LineDataSet>();
+    public LineData lineData=null;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.liner_chart, container, false);
+        lineChart= (LineChart) view.findViewById(R.id.spread_line_chart);
+        LineData resultLineData=getLineData(40, 100);
+        showChart();
+        return view;
+    }
+    /**gv
+     * 初始化数据
+     * count 表示坐标点个数，range表示等下y值生成的范围
+     */
+    public LineData getLineData(int count,float range){
+        for(int i=0;i<count;i++){  //X轴显示的数据
+            x.add(i+"");
+        }
+        for(int i=0;i<count;i++){//y轴的数据
+            float result=(float)(Math.random()*range)+3;
+            y.add(new Entry(result,i));
+        }
+        LineDataSet lineDataSet=new LineDataSet(y,"折线图");//y轴数据集合
+        lineDataSet.setLineWidth(1f);//线宽
+        lineDataSet.setCircleSize(2f);//现实圆形大小
+        lineDataSet.setColor(Color.RED);//现实颜色
+        lineDataSet.setCircleSize(Color.BLUE);//圆形颜色
+        lineDataSet.setHighLightColor(Color.WHITE);//高度线的颜色
+        lineDataSets.add(lineDataSet);
+        lineData=new LineData(x,lineDataSets);
+        return lineData;
+    }
+    /**
+     * 设置样式
+     */
+    public void showChart(){
+        lineChart.setDrawBorders(false);//是否添加边框
+        lineChart.setDescription("有风险的数据");//数据描述
+        lineChart.setNoDataTextDescription("我需要数据");//没数据显示
+        lineChart.setDrawGridBackground(true);//是否显示表格颜色
+        lineChart.setBackgroundColor(Color.YELLOW);//背景颜色
+        lineChart.setData(lineData);//设置数据
+        Legend legend=lineChart.getLegend();//设置比例图片标示，就是那一组Y的value
+        legend.setForm(Legend.LegendForm.CIRCLE);//样式
+        legend.setFormSize(6f);//字体
+        legend.setTextColor(Color.WHITE);//设置颜色
+        lineChart.animateX(2000);//X轴的动画
+    }
+}
